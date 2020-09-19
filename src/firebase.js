a34424353848d1b0b33a55c71da790e80db025df
Yashika Bihani <yashikabihani7@gmail.com>
2020-09-20 01:49:45 +0530

    Header Logo + Search Icon

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBv8uHipJ0W1-BT9Eu5OsP597gdldbl7ZA",
+    authDomain: "clone-f2fc6.firebaseapp.com",
+    databaseURL: "https://clone-f2fc6.firebaseio.com",
+    projectId: "clone-f2fc6",
+    storageBucket: "clone-f2fc6.appspot.com",
+    messagingSenderId: "190976055998",
+    appId: "1:190976055998:web:486d567cba70bc2cbbdcef",
+    measurementId: "G-M9GVWVH1DY"
+  };
