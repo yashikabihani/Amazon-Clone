56848f760ac650af8d5b66be0923711f8eef2945
Yashika Bihani <yashikabihani7@gmail.com>
2020-09-20 13:54:05 +0530

    Products' Component UI

--- src/Header.js
+++ src/Header.js
@@ -30,7 +30,7 @@ function Header() {
 
         <div className="header__optionBasket">
             <ShoppingBasketIcon />
-        
+            <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>
---
+++ src/Product.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Product.css";
+
+function Product() {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>The Lean StartUp</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>29.99</strong>
+        </p>
+        <div className="product__rating">
+          <p>*</p>
+        </div>
+      </div>
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+        alt=""
+      />
+      <button>Add to Basket</button>
+    </div>
+  );
+}
+
+export default Product;
